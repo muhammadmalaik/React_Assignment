befbf7528f007b197da2b891270b6cedb714a842
Muhammad-Malaik <muhammadmalaik76@gmail.com>
2024-12-15 15:59:48 +0500

    Add files via upload

---
+++ Assignment 01/src/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const rootDiv = document.getElementById('root');
+
+ReactDOM.createRoot(rootDiv).render(
+  <div>
+    <h1>REACT</h1>
+    <p>React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”. We'll get to the funny XML-like tags soon. We use components to tell React what we want to see on the screen.
+    </p>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0jQPcLR2zDp6yPjuN6OqywK4v0ybNPxu1kw&s" alt="" />
+    <p>React is a declarative, efficient, and flexible JavaScript library for building user interfaces. It lets you compose complex UIs from small and isolated pieces of code called “components”. We'll get to the funny XML-like tags soon. We use components to tell React what we want to see on the screen.</p>
+    <img src="https://www.patterns.dev/img/reactjs/react-logo@3x.svg" alt="" width={300}/>
+  </div>
+)
